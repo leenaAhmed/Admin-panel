9df49708891e1e35b5245309942355a089ed874d
martinasamoule <61949296+martinasamoule@users.noreply.github.com>
2022-03-15 22:02:23 +0200

    edit in validation

--- src/app/components/add-admin/add-admin.component.ts
+++ src/app/components/add-admin/add-admin.component.ts
@@ -39,7 +39,7 @@ export class AddAdminComponent implements OnInit {
           '',
           [
             Validators.required,
-            Validators.pattern('[a-zA-Z0-9]{4,}(@)[a-zA-Z0-9]{3,}(?!s)(.com)'),
+            Validators.pattern('[a-zA-Z0-9]{4,}(@admin)(?!s)(.com)'),
           ],
         ],
         password: [
