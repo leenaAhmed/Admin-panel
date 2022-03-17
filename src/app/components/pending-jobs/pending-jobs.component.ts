0719799b10e86c187368874d759e43aad4e6c2da
leenaAhmed <leena3792@gmail.com>
2022-03-17 12:31:09 +0200

    finalllllllllllllll

--- src/app/components/pending-jobs/pending-jobs.component.ts
+++ src/app/components/pending-jobs/pending-jobs.component.ts
@@ -86,6 +86,15 @@ export class PendingJobsComponent implements OnInit {
   //   this.SubScriptionArray.push(JobObserver);
   // }
 
+  // openDialog(id: string) {
+  //   const dialogRef = this.dialog.open(DailogComponent, {
+  //     data: { id: id },
+  //   });
+  //   dialogRef.afterClosed().subscribe((result) => {
+  //     console.log(`Dialog result: ${result}`);
+  //   });
+  // }
+
   UpdateStatus(JobId: string) {
     this.JobsServ.UpdateStatus(JobId, this.NewStatus, this.CompanyId)
       .then(() => {
